e38fd0a77d8c53e2034961fa6db8f1f2771eb3d5
mrDock21 <dan.mejia@alumnos.udg.mx>
2021-07-07 19:55:29 -0500

    Basic animations for command windows

--- PlainProject/scenes/ui/ui_elements/CmdWindow.gd
+++ PlainProject/scenes/ui/ui_elements/CmdWindow.gd
@@ -7,6 +7,8 @@ signal move_to_front_requested(_window)
 export (String) var title = "" setget set_title
 export (String, MULTILINE) var text_content = "" setget set_text_content
 
+onready var tween = $Tween
+
 
 func _gui_input(_event : InputEvent) -> void:
 	if _event is InputEventMouseButton and _event.button_index == BUTTON_LEFT and _event.pressed:
@@ -24,4 +26,27 @@ func set_text_content(_text_content : String) -> void:
 
 
 func _on_CloseButton_pressed():
+	var currPos = self.rect_position;
+	var animTime = 0.25
+	# movement anim
+	tween.interpolate_property(
+		self, "rect_position",
+		# jusy go upwards and right in 0.5s
+		currPos, currPos + Vector2(0, -20), animTime,
+		Tween.TRANS_LINEAR, Tween.EASE_OUT
+	)
+	# alpha anim
+	tween.interpolate_property(
+		self, "modulate:a",
+		# jusy go upwards and right in 0.5s
+		1, 0, animTime,
+		Tween.TRANS_LINEAR, Tween.EASE_OUT
+	)
+	tween.start()
+	# wait until it ends...
+	yield(get_tree().create_timer(animTime), "timeout")
+	
 	hide()
+	# return to old state after anim
+	self.rect_position = currPos
+	self.modulate.a = 1
