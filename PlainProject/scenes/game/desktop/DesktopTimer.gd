ed2c014b43f072875967decdf573b61306b5676c
mrDock21 <dan.mejia@alumnos.udg.mx>
2021-07-08 00:05:46 -0500

    Added "last 10 seconds" sound effect and timer

--- PlainProject/scenes/game/desktop/DesktopTimer.gd
+++ PlainProject/scenes/game/desktop/DesktopTimer.gd
@@ -8,15 +8,13 @@ const TIME_FORMAT = "%02d:%02d"
 export (float) var countdown_time = 900 # 15 min
 export (float) var queue_freuqency = 60
 
-
-#func _ready() -> void:
-#	start()
-
+var lastTenSeconds = false
 
 func start() -> void:
 	# +1 to sync with sound queue
 	$CountdownTimer.start(countdown_time + 1)
 	$SoundQueueTimer.start(queue_freuqency)
+	$BombTickTimer.start()
 	$AudioStreamPlayer2D.play()
 	show()
 
@@ -33,7 +31,7 @@ func _process(_delta : float) -> void:
 		var seconds = (time % 60)
 		var minutes = int(time / 60)
 		$Margin/TimeLabel.text = TIME_FORMAT % [minutes, seconds]
-
+		lastTenSeconds = minutes <= 0 and seconds < 11
 
 func _on_CountdownTimer_timeout():
 	emit_signal("timeout")
@@ -41,3 +39,9 @@ func _on_CountdownTimer_timeout():
 
 func _on_SoundQueueTimer_timeout():
 	$AudioStreamPlayer2D.play()
+
+
+func _on_BombTickTimer_timeout():
+	if lastTenSeconds:
+		$AudioBombTick.play()
+	
