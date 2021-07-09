9970a9a333ee399309afa7e773f1b1d673342e83
mrDock21 <dan.mejia@alumnos.udg.mx>
2021-07-09 02:25:31 -0500

    Added animation to title screen

--- PlainProject/scenes/screens/MainMenuScreen.gd
+++ PlainProject/scenes/screens/MainMenuScreen.gd
@@ -1,5 +1,25 @@
 extends Control
 
+func _ready():
+	start_animation()
+
+func start_animation():
+	var screen = $Margin
+	var tween = $Tween
+	
+	yield(get_tree().create_timer(1), "timeout")
+	
+	screen.rect_pivot_offset = screen.rect_size / 2
+	screen.rect_scale = Vector2(0, 0)
+	screen.show()
+	
+	tween.interpolate_property(
+		screen, "rect_scale",
+		Vector2(0, 0), Vector2(1, 1), 1,
+		Tween.TRANS_ELASTIC, Tween.EASE_IN_OUT
+	)
+	tween.start()
+	#yield(get_tree().create_timer(1), "timeout")
 
 func _on_PlayButton_pressed():
 	var _error = get_tree().change_scene(ScenePaths.INTRO_SCENE)
